99a1aa77a7e9e53240c6db36c2a438f2166d0cf5
Parul76 <parulmittal27@gmail.com>
2021-10-15 11:06:34 +0200

    Add invalid test case

---
+++ test/specs/basictest.failure.js
@@ -0,0 +1,9 @@
+const campaignWebpage = require('../pageobjects/campaignWebpage');
+const allureReporter = require('@wdio/allure-reporter')
+
+describe('Invalid test cases for my campaign website', () => {
+    it('should do some assertions for success', async () => {
+        await browser.url('https://www.volvocars.com/intl/v/car-safety/a-million-more')
+        await expect(browser).toMatch('fail')
+    })
+ });
--- test/specs/basictest.success.js
+++ test/specs/basictest.success.js
@@ -1,17 +1,13 @@
 const campaignWebpage = require('../pageobjects/campaignWebpage');
 const allureReporter = require('@wdio/allure-reporter')
 
-
-
-describe('my campaign website', () => {
-    it('should do some assertions', async () => {
+describe('Valid test cases for my campaign website', () => {
+    it('should do some assertions for failure', async () => {
         await browser.url('https://www.volvocars.com/intl/v/car-safety/a-million-more')
         await expect(browser).toHaveUrlContaining('volvo')
         await expect(browser).toHaveTitle('A million more | Volvo Cars - International')
         await expect(browser).toHaveTitleContaining('Volvo')   
-        
-    })
-  
+    })  
  });
 
 
