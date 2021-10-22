a147165085632d226be8ca9233c83d8ce65f6a7a
Parul76 <parulmittal27@gmail.com>
2021-10-22 16:07:11 +0200

    some updates

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,36 +1,39 @@
-const allure = require('allure-commandline')
 exports.config = {
  
-    framework: 'mocha',
+framework: 'mocha',
+bail: 0,
+baseUrl: 'https://www.volvocars.com/intl/v/car-safety/a-million-more',
+waitforTimeout: 1000000,
+connectionRetryTimeout: 120000,
+connectionRetryCount: 3,
+services: ['chromedriver'],
+reporters: [[ 'spec',{
+outputDir: 'spec-results',
+disableWebdriverStepsReporting: true,
+disableWebdriverScreenshotsReporting: true,
+}]],
+reporters: [['allure', {
+outputDir: 'allure-results',
+disableWebdriverStepsReporting: true,
+disableWebdriverScreenshotsReporting: true,
+}]],
+
+specs: [
+'./test/specs/**/*.js'
+],
+
+suites: {
     specs: [
-        './test/specs/**/*.js'
-    ],
-    suites: {
-        specs: [
             './test/specs/basicTest.js',
             './test/specs/basictest.failure.js'
-        ],
-        otherFeature: [
-            // ...
-        ],
-        
+            ],
+
     }, 
     capabilities: [{
         maxInstances: 10,
         browserName: 'chrome',
     }],
-    bail: 0,
-    baseUrl: 'https://www.volvocars.com/intl/v/car-safety/a-million-more',
-    waitforTimeout: 1000000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    services: ['chromedriver'],
-    reporters: [ 'spec'],
-    reporters: [['allure', {
-    outputDir: 'allure-results',
-    disableWebdriverStepsReporting: true,
-    disableWebdriverScreenshotsReporting: true,
-}]],
+   
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
